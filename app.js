d3101f45707a48fd1c235c57f7f5cf8d75fbb69d
Federico Callegari <callegarifede1@gmail.com>
2024-07-10 17:45:42 -0300

    Add buton clasName  app.js

--- app.js
+++ app.js
@@ -62,16 +62,18 @@ let carrito = [];
 
 productos.forEach((product)=>{
     let content = document.createElement("div");
+    content.className = "card";
     content.innerHTML = `  
         <img src = "${product.img}">
         <h3>${product.nombre}</h3>
         <p>${product.talle}</p>
-        <p>${product.precio} $</p>
+        <p class="price">${product.precio} $</p>
     `;
 
     shopContent.append(content);
 
     let comprar = document.createElement("button");
     comprar.innerText = "comprar";
+    comprar.className ="comprar";
     content.appendChild(comprar);
 })
